9b4a637625e5de5077b17f671acee389e130f51d
unknown <freekodelka86@gmail.com>
2023-04-09 21:29:19 +0300

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 100;
+int b = 13;
+
+int max = a;
+
+if (a < b) max = b;
+
+Console.Write("max = :");
+Console.WriteLine(max);
---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1200;
+int b = 150;
+int c = 300000;
+
+int max = a;
+
+if (b > a) max = b;
+if (c > a) max = c;
+
+
+Console.Write("max = :");
+Console.WriteLine(max);
